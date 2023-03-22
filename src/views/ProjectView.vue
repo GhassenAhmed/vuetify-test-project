8b433cb290b0936bf72686f67c72fd4e9862f322
ghassen <ghassenahmed74@gmail.com>
2023-03-22 23:35:15 +0100

    add expansions + filter computed

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -2,7 +2,43 @@
   <div class="project">
     <h1 class="subheading grey--text">Project</h1>
     <v-container class="my-5"> 
-      content
+        <v-expansion-panels v-for="project in MyProjects" :key="project.person">
+          <v-expansion-panel>
+            <v-expansion-panel-header>
+              {{ project.title }}
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-card flat>
+                <v-card-text>
+                  <div class="font-weight-bold">{{project.due}}</div>
+                  <div>{{project.content}}</div>
+                </v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
     </v-container>
   </div>
 </template>
+<script>
+  export default {
+    data(){
+      return{
+        projects:[
+          {title:'Design Etudiant page',person:'Ghassen Ahmed',due:'22 mars 2023',status:'Ongoing',content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque doloribus fugit magnam aliquam quos nesciunt repellat perferendis quibusdam recusandae commodi.'},
+          {title:'Code up the home page',person:'Malek Naamen',due:'19 mars 2023',status:'Complete',content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque doloribus fugit magnam aliquam quos nesciunt repellat perferendis quibusdam recusandae commodi.'},
+          {title:'Design chef department page  ',person:'Hkimi Amine',due:'15 mars 2023',status:'Complete',content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque doloribus fugit magnam aliquam quos nesciunt repellat perferendis quibusdam recusandae commodi.'},
+          {title:'Authentification',person:'Talel Mejri',due:'25 mars 2023',status:'Overdue',content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque doloribus fugit magnam aliquam quos nesciunt repellat perferendis quibusdam recusandae commodi.'},
+          {title:'Design Etudiant page',person:'Ghassen Ahmed',due:'22 mars 2023',status:'Ongoing',content:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque doloribus fugit magnam aliquam quos nesciunt repellat perferendis quibusdam recusandae commodi.'}
+        ]
+      }
+    },
+    computed:{
+      MyProjects(){
+        return this.projects.filter(project=>{
+          return project.person==='Ghassen Ahmed'
+        })
+      }
+    }
+  }
+</script>
