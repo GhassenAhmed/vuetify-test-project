88d6b2aeaac8c2e48af782827af8bf6ffed36485
ghassen <ghassenahmed74@gmail.com>
2023-03-23 00:02:43 +0100

    add dropdown menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,10 +7,36 @@
             </v-btn>
         </v-toolbar-side-icon>
         <v-toolbar-title class="text-uppercase grey--text">
-            <span class="font-weight-light">todo</span>
-            <span >ghassen</span>
+            <div class="hidden-md-and-down">
+                <span class="font-weight-light">todo</span>
+                <span >ghassen</span>
+            </div>
         </v-toolbar-title>
         <v-spacer></v-spacer>
+        <v-menu offset-y >
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                color="grey lighten-2"
+                flat
+                dark
+                v-bind="attrs"
+                v-on="on"
+                class="mr-4"
+                >
+                <v-icon left>mdi-arrow-down-bold-outline</v-icon>
+                <span class="white--text">Menu</span>
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item
+                   v-for="link in links" 
+                   :key="link.text"
+                   router :to="link.route"
+                >
+                <v-list-item-title>{{link.text}}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
         <v-btn  flat color="grey  lighten-3  grey--text">
             <span>Sign out</span>
             <v-icon class="px-3">mdi-account-arrow-right-outline</v-icon>
